7a774d34807fb4cc94a4d5b813275b96f7bf1be2
Matteo Contu <m.contu@live.it>
2023-02-03 21:18:37 +0100

    event system imported

---
+++ Assets/Matteo/CodeBase/Scripts/Events/StandardListener.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeBase.Events;
+[System.Serializable]
+public class TestData : EventData
+{
+
+}
+public class StandardListener : GameEventListener<TestData>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
