d03a8bd602ee49d4cb7c27c5ceff51ab4279a286
Matteo Contu <m.contu@live.it>
2023-02-04 23:00:52 +0100

    centered position

--- Assets/Matteo/Tests/RootTile.cs
+++ Assets/Matteo/Tests/RootTile.cs
@@ -8,6 +8,7 @@ public class RootTile : Tile
     public RootTileController tileController;
     public RootTileData.TileState tileState;
 
+
     [HideInInspector] public RootTileData tileData;
     public PrefabsSO gamePrefabs;
 
@@ -20,11 +21,24 @@ public class RootTile : Tile
 
         //tileController = new RootTileController(spawner);
         Vector2Int position = new Vector2Int(location.x, location.y);
+        // TileData myTileData = new TileData();
+        // GetTileData(location, tilemap, ref myTileData);
+        // Vector3 tileLocalPosition = myTileData.transform.GetColumn(3);
+        // Debug.LogWarning(tileLocalPosition);
         //Debug.LogWarning(position);
         tileController.tileData.SetPosition(position);
         tileController.tileData.spawnPosition = this.transform.GetPosition();
         tileController.tileData.spawnPosition.z = -10;
         tileController.tileData.tileState = tileState;
+        if (location.x < RootMap.Instance().minGridWidth)
+            RootMap.Instance().minGridWidth = location.x;
+        if (location.x > RootMap.Instance().maxGridWidth)
+            RootMap.Instance().maxGridWidth = location.x;
+        if (location.y < RootMap.Instance().minGridHeight)
+            RootMap.Instance().minGridHeight = location.y;
+        if (location.y > RootMap.Instance().maxGridHeight)
+            RootMap.Instance().maxGridHeight = location.y;
+
         if (!RootMap.Instance().myTiles.ContainsKey(position))
             RootMap.Instance().myTiles.Add(position, tileController);
 
